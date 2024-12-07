30911332334d804f54c289b39ff46f82218d6d1f
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2024-12-07 08:45:35 +0100

    Einbindung der benötigten Bibliotheken

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include <Arduino.h>
+
+// LCD Display
+#include <LiquidCrystal_I2C.h>
+// I2C Komunikation
+#include <Wire.h>
+// Bosch Sensor
+#include <Adafruit_BME680.h>
+// Kommunikation mit BME 680. Wird nur verwendet, wenn I2C nicht geht
+#include <SPI.h>
+// ??? Verwendung noch unklar
+#include <Adafruit_Sensor.h>
+// Komunikation über Modem oder Bluetooth
+#include <SoftwareSerial.h>
+// Kameramodul
+#include <Adafruit_OV7670.h>
+
+
+
+
+
 void setup() {
 // write your initialization code here
 }
