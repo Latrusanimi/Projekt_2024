349cdf8a5c9a0698830e1844e02ab86ab0021b1f
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2024-12-30 18:00:32 +0100

    Initialisierung WLAN

--- src/main.cpp
+++ src/main.cpp
@@ -28,41 +28,48 @@ Adafruit_BME680 bme680;
 // Definition welches Display verwendet wird.
 LiquidCrystal_I2C lcd(0x27,20,4);
 
-// Anschluss SIM7600g-h Modul definieren
-//SoftwareSerial sim7600g(11,10);
+// WLAN Daten
+const char* ssid = "HUAWEI P10 Plus 2";
+const char* password = "fdc3133fc79e";
 
 // ThingSpeak API Keys
 const char* api_key_bme680 = "VFNZDUII0ENDF526";  // Sicherheitsrisiko, da API Key unverschlüssselt. in einer späteren Version anzupassen
-
 // URL Thingspeak
 const char* url_bme680 = "http://api.thingspeak.com/update";
 
+// Definition Funktion WLAN Verbindungsaufbau
+void verbindungWlan();
+
+// Definition Funktion AT Befehle senden und Antwort auswerten
+bool sendAT(const char* befehl, const char* antwortErwartet, unsigned long timeout);
 
 // Definition Funktion Daten senden
 void sendBME680Data(float temperature, float humidity, float pressure);
 
 // Definition Funktion 2004 Display
-void anzeigeDisplay(float temperatur, float feuchtigkeit, float luftdruck, bool statusVerbindung);
+void anzeigeDisplay(float temperatur, float feuchtigkeit, float luftdruck, String antwortESP01);
 
-bool netzwerkTest ();
+//bool netzwerkTest ();
 
 void setup() {
-// write your initialization code here
+    // write your initialization code here
 
     //sim7600g.begin(19200);
     Wire.begin();
 
 
-// LCD initialisieren
+    // LCD initialisieren
     lcd.init();
     lcd.backlight();
 
-// PIN Mode definieren
-   /* pinMode(2,OUTPUT); // rote LED
+    // PIN Mode definieren
+    // Pins werden später benötigt
+    /*
+    pinMode(2,OUTPUT); // rote LED
     pinMode(3,OUTPUT); // gelbe LED
-    pinMode(4,OUTPUT); // grüne LED */
+    pinMode(4,OUTPUT); // grüne LED*/
 
-// BME 680 initialisieren
+    // BME 680 initialisieren
     // BME680_OS_8X bedeutet, dass 8 Messungen durchgeführt werden und der Mittelwert genommen wird.
     // es sind 0, 1, 2, 4, 8 und 16 Messungen pro Abfrage möglich.
     bme680.setHumidityOversampling(BME680_OS_8X);
@@ -73,21 +80,40 @@ void setup() {
     bme680.setIIRFilterSize(BME680_FILTER_SIZE_3);
     bme680.begin(BME680_I2C_ADDRESS);
 
-// Geschwindigkeit der seriellen Verbindung definieren
-   Serial.begin(115200);
+    // Geschwindigkeit der seriellen Verbindung definieren
+    Serial.begin(115200);
+
+    // Verbindungsaufbau
+    lcd.setCursor(0,0);
+    lcd.print("Initialisierung");
+    delay(1000);
+    verbindungWlan();
+
+    // Timer damit WLAN Zeit hat für Verbindungsaufbau
+    for (int i = 5; i >= 0; i--) {
+        delay(1000); // 1 Sekunde warten
+        lcd.setCursor(0, 2);
+        lcd.print("                "); // Zeile leeren
+        lcd.setCursor(0, 2);
+        lcd.print(i);
+    }
+    delay(1000);
+    lcd.clear();
 }
+
 
 void loop() {
     // write your code here
     bme680.performReading();
 
-    bool statusVerbindung = netzwerkTest();
+    //bool statusVerbindung = netzwerkTest();
     float temperatur = bme680.readTemperature();
     float feuchtigkeit = bme680.readHumidity();
     float luftdruck = bme680.readPressure()/100000;
+    String antwortESP01 = "";
 
     // Ausgabe auf Display
-    anzeigeDisplay(temperatur, feuchtigkeit, luftdruck, statusVerbindung);
+    anzeigeDisplay(temperatur, feuchtigkeit, luftdruck, antwortESP01);
 
     delay(10000);
 
@@ -100,6 +126,41 @@ void loop() {
 
 }
 
+void verbindungWlan() {
+    sendAT("AT","OK",1000);
+    sendAT("AT+CWMODE=1","OK",1000);
+    String befehl = String("AT+CWJAP=\"") + String(ssid) + String("\",\"") + String(password) + String("\"");
+    sendAT(befehl.c_str(),"WIFI CONNECTED",10000);
+    lcd.setCursor(0,3);
+    lcd.print("WLAN Verbunden");
+    }
+
+bool sendAT(const char* befehl, const char* antwortErwartet, unsigned long timeout) {
+    Serial.println(befehl);
+    unsigned long start = millis();
+
+    String antwort = "";
+    while(millis() - start < timeout) {
+        if(Serial.available()) {
+            char c = Serial.read();
+            antwort += c;
+
+            if(antwort.indexOf(antwortErwartet) != -1) {
+                lcd.setCursor(0,2);
+                lcd.print("Antwort WLAN-Modul:");
+                lcd.setCursor(0,3);
+                lcd.print(antwort);
+                return true;
+            }
+        }
+    }
+    lcd.setCursor(0,2);
+    lcd.print("Antwort WLAN-Modul:");
+    lcd.setCursor(0,3);
+    lcd.print("ERROR 404");
+    return false;
+}
+
 // Funktion Daten an Server senden
 void sendBME680Data(float temperature, float humidity, float pressure) {
     String url = String(url_bme680) + "?api_key=" + api_key_bme680 +
@@ -107,36 +168,11 @@ void sendBME680Data(float temperature, float humidity, float pressure) {
                  "&field2=" + String(humidity) +
                  "&field3=" + String(pressure);
 
-    // APN für Netzzugang in Modem setzen
-    //sim7600g
-    //Serial.println("AT+CGDCONT=1,\"IP\",\"gprs.swisscom.ch\"");
-    delay(500);
-    //sim7600g
-    Serial.println("AT+CGATT=1");
-    delay(500);
-    //sim7600g
-    Serial.println("AT+CGACT=1,1");
-    delay(500);
-    //sim7600g
-    Serial.println("AT+HTTPPARA=\"CID\",1");
-    delay(500);
 
-    // Eigentlicher HTTP aufruf
-    //sim7600g
-    Serial.println("AT+HTTPINIT");
-    delay(500);
-    //sim7600g
-    Serial.println("AT+HTTPPARA=\"URL\",\"" + url + "\"");
-    delay(500);
-    //sim7600g
-    Serial.println("AT+HTTPACTION=0");
-    delay(3000);
-    //sim7600g
-    Serial.println("AT+HTTPTERM");
 }
 
 // Funktion Displayanzeige
-void anzeigeDisplay(float temperatur, float feuchtigkeit, float luftdruck, bool statusVerbindung) {
+void anzeigeDisplay(float temperatur, float feuchtigkeit, float luftdruck, String antwortESP01) {
     lcd.clear();
     // Zeile 1
     lcd.setCursor(0, 0);
@@ -154,47 +190,6 @@ void anzeigeDisplay(float temperatur, float feuchtigkeit, float luftdruck, bool 
     lcd.print(luftdruck);
     lcd.print(" bar");
     // Zeile 4
-    lcd.setCursor(0, 3);
-    /*if (statusVerbindung) {
-        lcd.print("Verbindung gut");
-    }
-    else {
-        lcd.print("Verbindung nicht gut");
-    }*/
-    lcd.print(statusVerbindung);
-}
-
-// Funktion Verbindungsstatus testen
-bool netzwerkTest () {
-    String antwort ="";
-    //sim7600g
-    Serial.println("AT+CGREG?");
-    delay(500);
-    while (/*sim7600g*/Serial.available()) {
-        char c = /*sim7600g*/Serial.read();
-        antwort += c;
-    }
-
-    /*lcd.clear();
-    lcd.setCursor(0, 0);
-    if (antwort.length() > 20) {
-        lcd.print(antwort.substring(0, 20));
-        lcd.setCursor(0, 1);
-        lcd.print(antwort.substring(20, 40));
-    } else {
-        lcd.print(antwort);
-    }*/
-
-    int antwortpositiv = antwort.indexOf("+CGREG:");  // Prüfen, ob die Antwort vom SIM7600g-h positiv ist
-    if (antwortpositiv != -1) {
-        // Antwort nach dem letzten Komma auswerten. ,1 bedeutet: regristriert im Heimnetzwerk. ,5 bedeutet: regristriert, roaming.
-        int letztesKomma = antwort.lastIndexOf(',');
-        if (letztesKomma != -1 && (letztesKomma +1) < (int)antwort.length()) {
-            char status = antwort[letztesKomma + 1];
-            if ( status == '1' || status == '5') {
-                return true;
-            }
-        }
-    }
-    return false;
+    //lcd.setCursor(0, 3);
+    //lcd.print(antwortESP01);
 }
